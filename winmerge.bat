0c96b2b8e3e79043a0f9439cc404f90ec681c6a7
bnjay <easyklick@outlook.com>
2022-06-24 10:34:44 +0900

    added patters and skipped folder count

--- winmerge.bat
+++ winmerge.bat
@@ -1,14 +1,26 @@
-:: Compare two files in a directory and produce an HTML report.
+@echo off
+rem Compare two files in a directory and produce an HTML report.
+rem
+rem If there are more than two files the directory is skipped.
+rem An HTML report is produced even if there is no difference. However,
+rem Chrome won't open it. You can adjust the naming patterns for directories
+rem and files.
+rem
+rem Dependencies: WinMerge and Chrome
+rem 
 
 @echo off
 set TEST_DIR=C:\Tools\test
-set exe_file="C:\Tools\WinMerge\WinMergeU.exe"
+set DIR_PATTERNS=xxx.yyy.*
+set FILE_PATTERNS=*example*
+set EXE_FILE=C:\Tools\WinMerge\WinMergeU.exe
 set folder_count=0
+set skipped_folder_count=0
 
 rem Loop through all directories
 setlocal enabledelayedexpansion
 
-for /D %%G in (%TEST_DIR%\xxx.yyy.*) do (
+for /d %%G in (%TEST_DIR%\%DIR_PATTERNS%) do (
 
 set dir=%%G
 rem echo !dir!
@@ -25,14 +37,14 @@ set file_count=0
 setlocal enabledelayedexpansion
 set too_many_files=false
 
-for /f %%i in ('dir /b /s !dir!') do (
+for /f "delims=" %%i in ('dir /b /s !dir!\!FILE_PATTERNS!') do (
   if !file_count! == 2 call:error !dir!
   rem echo %%i
   
   if !left_folder! == "" (
-    set left_folder=%%i
+    set left_folder="%%i"
   ) else if !right_folder! == "" (
-    set right_folder=%%i
+    set right_folder="%%i"
   )
   set /a file_count=file_count+1
   rem echo !file_count! !left_folder! !right_folder!
@@ -40,21 +52,19 @@ for /f %%i in ('dir /b /s !dir!') do (
 
 @echo off
 
-rem set left_folder="C:\Tools\test\xxxyyyzzz\Excel_conversion_macro_example_test.xlsm"
-rem set right_folder="C:\Tools\test\xxxyyyzzz\Excel_conversion_macro_example2_test.xlsm"
-
+rem echo(
 rem echo !left_folder!
 rem echo !right_folder!
 
 if exist !dir!_diff.html del !dir!_diff.html
 
-if !too_many_files! == false start /wait "" !exe_file! /r /wl /wr !left_folder! !right_folder! -minimize -noninteractive -u -x -or !dir!_diff.html & call:check_diff_report !dir!_diff.html
+if !too_many_files! == false start /wait "" !EXE_FILE! /r /wl /wr ""!left_folder!"" ""!right_folder!"" -minimize -noninteractive -u -x -or !dir!_diff.html & call:check_diff_report !dir!_diff.html
 
 )
 
 echo(
 echo(
-echo Finished all %folder_count% folders.
+echo Checked %folder_count% folders and skipped %skipped_folder_count%.
 
 exit /b
 
@@ -63,6 +73,7 @@ echo(
 echo(
 echo Too many files found in the directry %1
 set too_many_files=true
+set /a skipped_folder_count=skipped_folder_count+1
 exit /b
 
 :check_diff_report
@@ -70,6 +81,7 @@ exit /b
 echo(
 echo(
 echo "Checking the diff report" %1
+echo(
 
 findstr href %1 
 if %errorlevel% equ 0 echo "Diff found" & start chrome.exe file://%1
